c851a25756025750a60991fddbc32751df842128
varalakshmi <varalakshmi@77.com>
2022-07-05 09:18:02 +0000

    key

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.css'
 
 const userDetailsList = [
   {
+    uniqueNo: 1,
     url: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
     name: 'Rahul',
     role: 'Software Developer',
   },
   {
+    uniqueNo: 2,
     url: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
     name: 'floyd',
     role: 'Software Developer',
   },
   {
+    uniqueNo: 3,
     url: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
     name: 'Jacob',
     role: 'Software Developer',
@@ -24,7 +27,7 @@ const App = () => (
     <h1 className="title">User List</h1>
     <ul>
       {userDetailsList.map(eachItem => (
-        <UserProfile userDetails={eachItem} />
+        <UserProfile userDetails={eachItem} key={eachItem.uniqueNo} />
       ))}
     </ul>
   </div>
